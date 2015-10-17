21b6bf15c1b1ac14f859c234ef3221820f94ee48
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-17 16:21:44 +0530

    Add skeleton BowlingGameTests.cpp file

---
+++ BowlingGameKataTests/BowlingGameTests.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <cppUnit/TestCase.h>
+#include <cppUnit/extensions/HelperMacros.h>
+
+#include "TestFixtureMacro.h"
+
+TEST_FIXTURE(BowlingGameTests, NoBallsRolledShouldScore0);
+
+void BowlingGameTests::NoBallsRolledShouldScore0()
+{
+}
