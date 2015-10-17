fe5d344375fad2be3aa815d518c833ad926c5a1b
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-17 17:13:41 +0530

    *Step 5* Red: failing test for 20 rolls of 1 scores 20

--- BowlingGameKataTests/BowlingGameTests.cpp
+++ BowlingGameKataTests/BowlingGameTests.cpp
@@ -5,7 +5,8 @@
 #include "TestFixtureMacro.h"
 #include "../BowlingGameKata/BowlingGame.h"
 
-TEST_FIXTURE(BowlingGameTests, GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0);
+TEST_FIXTURE(BowlingGameTests, GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0,
+    GivenTwenty1sBowled_WhenGameScoreCalculated_ShouldBe20);
 
 void BowlingGameTests::GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0()
 {
@@ -21,4 +22,20 @@ void BowlingGameTests::GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0()
     // Then
     int score = game.Score();
     CPPUNIT_ASSERT_EQUAL(0, score);
+}
+
+void BowlingGameTests::GivenTwenty1sBowled_WhenGameScoreCalculated_ShouldBe20()
+{
+    // Given
+    BowlingGame game;
+
+    // When
+    for (int i = 0; i < 20; ++i)
+    {
+        game.Roll(1);
+    }
+
+    // Then
+    int score = game.Score();
+    CPPUNIT_ASSERT_EQUAL(20, score);
 }
