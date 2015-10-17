4aab8fc35a3bc6b681cf10a6de1ed0af791fce25
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-17 16:58:36 +0530

    Change name of tests to given - when - then format

--- BowlingGameKataTests/BowlingGameTests.cpp
+++ BowlingGameKataTests/BowlingGameTests.cpp
@@ -4,8 +4,8 @@
 
 #include "TestFixtureMacro.h"
 
-TEST_FIXTURE(BowlingGameTests, When20GutterBallsRolled_ScoreShouldBe0);
+TEST_FIXTURE(BowlingGameTests, GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0);
 
-void BowlingGameTests::When20GutterBallsRolled_ScoreShouldBe0()
+void BowlingGameTests::GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0()
 {
 }
