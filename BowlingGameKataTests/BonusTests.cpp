6ef096d57a684ae22667f45945bc002bf176b571
Geoff Bones <geoff.bones@siemens.com>
2015-10-18 21:08:20 +0100

    Red: A bonus should expire

--- BowlingGameKataTests/BonusTests.cpp
+++ BowlingGameKataTests/BonusTests.cpp
@@ -5,11 +5,20 @@
 #include "TestFixtureMacro.h"
 #include "../BowlingGameKata/Bonus.h"
 
-TEST_FIXTURE(BonusTests, GivenBonusForOneBall_WhenBonusRequestedFirstTime_ShouldBeGranted);
+TEST_FIXTURE(BonusTests, GivenBonusForOneBall_WhenBonusRequestedFirstTime_ShouldBeGranted,
+    GivenBonusForOneBall_WhenBonusRequestedSecondTime_ShouldNotBeGranted);
 
 void BonusTests::GivenBonusForOneBall_WhenBonusRequestedFirstTime_ShouldBeGranted()
 {
     Bonus bonus(1);
     bool bonusGranted = bonus.GrantForOneBall();
     CPPUNIT_ASSERT_EQUAL(true, bonusGranted);
+}
+
+void BonusTests::GivenBonusForOneBall_WhenBonusRequestedSecondTime_ShouldNotBeGranted()
+{
+    Bonus bonus(1);
+    bonus.GrantForOneBall();
+    bool bonusGranted = bonus.GrantForOneBall();
+    CPPUNIT_ASSERT_EQUAL(false, bonusGranted);
 }
