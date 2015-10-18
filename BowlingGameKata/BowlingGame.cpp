5f1224929baac461188b414409c0a97d0c81c042
Geoff Bones <geoff.bones@siemens.com>
2015-10-18 14:58:45 +0100

    Implement bonus for a spare frame

--- BowlingGameKata/BowlingGame.cpp
+++ BowlingGameKata/BowlingGame.cpp
@@ -1,15 +1,19 @@
 #include "stdafx.h"
 #include "BowlingGame.h"
 
-
 BowlingGame::BowlingGame()
-    : m_RunningTotal(0)
+    : m_RunningTotal(0), m_FrameState(std::make_unique<FrameState>())
 {
 }
 
 void BowlingGame::Roll(int pins)
 {
+    if (m_FrameState->IsSpare())
+    {
+        m_RunningTotal += pins;
+    }
     m_RunningTotal += pins;
+    m_FrameState->Roll(pins);
 }
 
 int BowlingGame::Score()
--- BowlingGameKata/BowlingGame.h
+++ BowlingGameKata/BowlingGame.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <memory>
+#include "FrameState.h"
+
 class BowlingGame
 {
     int m_RunningTotal;
+    std::unique_ptr<FrameState> m_FrameState;
 public:
     BowlingGame();
     void Roll(int pins);
