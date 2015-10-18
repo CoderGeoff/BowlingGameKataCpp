e36be2ac110349984fa6c1fca53bafeae1e149ec
Geoff Bones <geoff.bones@siemens.com>
2015-10-18 22:39:38 +0100

    Use constant for number of balls in a frame

--- BowlingGameKata/FrameState.cpp
+++ BowlingGameKata/FrameState.cpp
@@ -3,6 +3,7 @@
 
 static const int MaximumFrameScore = 10;
 static const int FinalFrameNumber = 9;
+static const int MaximumBallCountInSingleFrame = 2;
 
 FrameState::FrameState() : m_CurrentFrameCount(0), m_CurrentFrameScore(0), m_BallCountInCurrentFrame(0)
 {
@@ -26,13 +27,13 @@ bool FrameState::IsStrike() const
 
 bool FrameState::IsSpare() const
 {
-    return m_CurrentFrameScore == MaximumFrameScore && m_BallCountInCurrentFrame == 2;
+    return m_CurrentFrameScore == MaximumFrameScore && m_BallCountInCurrentFrame == MaximumBallCountInSingleFrame;
 }
 
 bool FrameState::IsAtEndOfFrame() const
 {
     if (m_CurrentFrameCount == FinalFrameNumber) return false;
-    return m_BallCountInCurrentFrame == 2 || m_CurrentFrameScore == MaximumFrameScore;
+    return m_BallCountInCurrentFrame == MaximumBallCountInSingleFrame || m_CurrentFrameScore == MaximumFrameScore;
 }
 
 void FrameState::SetStateToStartOfFrame()
