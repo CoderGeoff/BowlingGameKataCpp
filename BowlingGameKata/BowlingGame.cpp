8246b1384737e2d91a00a45bae9f669b51ae5bf5
Geoff Bones <geoff.bones@siemens.com>
2015-10-18 21:23:47 +0100

    Major refactor of BowlingGame to use Bonus class

--- BowlingGameKata/BowlingGame.cpp
+++ BowlingGameKata/BowlingGame.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "BowlingGame.h"
+#include <algorithm>
 
 BowlingGame::BowlingGame()
     : m_RunningTotal(0), m_StrikeBonusActive(false), m_FrameState(std::make_unique<FrameState>())
@@ -8,25 +9,26 @@ BowlingGame::BowlingGame()
 
 void BowlingGame::Roll(int pins)
 {
-    if (m_FrameState->IsSpare())
-    {
-        m_RunningTotal += pins;
-    }
-    if (m_StrikeBonusActive)
-    {
-        m_RunningTotal += pins;
-        m_StrikeBonusActive = false;
-    }
-    if (m_FrameState->IsStrike())
-    {
-        m_RunningTotal += pins;
-        m_StrikeBonusActive = true;
-    }
+    m_RunningTotal += BonusFromSparesAndStrikes(pins);
     m_RunningTotal += pins;
     m_FrameState->Roll(pins);
+
+    if (m_FrameState->IsStrike() || m_FrameState->IsSpare())
+    {
+        int bonusBallCount = m_FrameState->IsSpare() ? 1 : 2;
+        m_Bonuses.push_back(std::make_shared<Bonus>(bonusBallCount));
+    }
 }
 
 int BowlingGame::Score()
 {
     return m_RunningTotal;
 }
+
+int BowlingGame::BonusFromSparesAndStrikes(int pins) const
+{
+    auto spareAndStrikeBonusCounter = [](const std::shared_ptr<Bonus>& bonus) -> bool { return bonus->GrantForOneBall(); };
+    int numberOfSpareAndStrikeBonusesActive = std::count_if(m_Bonuses.begin(), m_Bonuses.end(), spareAndStrikeBonusCounter);
+    return pins * numberOfSpareAndStrikeBonusesActive;
+}
+
--- BowlingGameKata/BowlingGame.h
+++ BowlingGameKata/BowlingGame.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <memory>
 #include "FrameState.h"
+#include "Bonus.h"
+#include <list>
 
 class BowlingGame
 {
     int m_RunningTotal;
     bool m_StrikeBonusActive;
     std::unique_ptr<FrameState> m_FrameState;
+    std::list<std::shared_ptr<Bonus>> m_Bonuses;
+    int BonusFromSparesAndStrikes(int pins) const;
 public:
     BowlingGame();
     void Roll(int pins);
